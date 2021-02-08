671d226138e785197742bb87a56a08ed89ea842b
Oscar Simpson <oscarsimpson1999@gmail.com>
2021-02-08 15:32:19 +0000

    Remove redundant code.

--- jacknife.py
+++ jacknife.py
@@ -23,30 +23,3 @@ with open("data/2pt_hisq_msml5_fine_D_nongold_489conf.gpl", "r") as textFile:
 	data = jacknife(raw_data)	
 	np.savetxt("data/jacknife.dat", data)
 
-if(false):
-	avg = data.mean(0)
-	stddev = data.std(0)
-
-	avg = np.append(avg, avg[0])
-	stddev = np.append(stddev, stddev[0])
-
-	fig, ax = plt.subplots(figsize=(12, 7.2))
-	ax.set_title("Average 2-point correlator for n=489 gauge configurations\non a fine lattice", fontsize=24)
-	ax.set_xticks(np.linspace(0, time_slices, 13))
-
-	ax.plot(range(time_slices+1), avg, 'b+', linewidth=0)
-	ax.set_xlabel("Time", fontsize=18)
-	ax.set_ylabel("Correlation", fontsize=18)
-	ax.tick_params(axis='both', which='both', labelsize=16)
-
-	ax2 = ax.twinx()
-	ax2.plot(stddev, color='orange', linewidth=0.8)
-	ax2.set_yscale('log')
-	ax2.set_ylabel('Error', fontsize=18)
-	ax2.tick_params(axis='both', which='both', labelsize=16)
-
-	fig.tight_layout()
-	plt.savefig("Correlator.png")
-
-	
-
